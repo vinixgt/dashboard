48dcd7f027adc038c1cf7d97382dce695d7c8134
Vinicio Rojas <rojas.gt@gmail.com>
2017-03-07 10:20:36 -0600

    otra forma usar las rutas
    eso es como un controlador

--- routes/cities.js
+++ routes/cities.js
@@ -1,9 +1,29 @@
+/*
 var express = require('express');
 var router = express.Router();
-
+*/
 /* GET cities listing. */
+/*
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
 module.exports = router;
+*/
+
+modules.exports = function (app, db) {
+  var HTTPStatus = require('http-status');
+  return {
+    root: function(req, res, next) {
+      db.City.findAll()
+      .then(function(cities){
+        return res.render('/views/cities.html',cities);
+      }).catch(function(error){
+        return res.status(HTTPStatus.INTERNAL_SERVER_ERROR)
+                  .json({'error': error});
+      });
+    },
+    otraRuta: function(req, res, next) {
+       return res.render('plantilla', datos);
+    }
+  } // return
+} // exports
